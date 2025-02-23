a899b80a29adcd6d29e37c3b66f08a281f00d4e1
ElisaCarrascoLanusse <ecarrascolanusse@gmail.com>
2025-02-22 18:54:42 -0600

    home.js

---
+++ scripts/home.js
@@ -0,0 +1,28 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const learnMoreButton = document.querySelector("#learn-more-btn");
+    if (learnMoreButton) {
+        learnMoreButton.addEventListener("click", () => {
+            event.preventDefault();
+            window.location.href = "about.html";
+        });
+    }
+
+    const projects = [
+        { id: "laboratoria-btn", url: "projects/work/laboratoria.html" },
+        { id: "memory-btn", url: "projects/work/memory.html"},
+        { id: "atip-btn", url: "projects/work/atip.html"}
+    ];
+
+    const viewProjectButtons = document.querySelectorAll(".view-project-btn");
+    viewProjectButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const project = projects.find(p => p.id === button.id);
+            if (project) {
+                window.location.href = project.url;
+            }
+        });
+    });    
+});
+
