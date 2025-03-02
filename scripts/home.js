07959554b91e5a72884ca07e2cb5f9eb6afa057c
ElisaCarrascoLanusse <ecarrascolanusse@gmail.com>
2025-03-01 18:28:22 -0600

    pls?

--- scripts/home.js
+++ scripts/home.js
@@ -5,12 +5,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (learnMoreButton) {
         learnMoreButton.addEventListener("click", () => {
             event.preventDefault();
-            window.location.href = "about.html";
+            window.location.href = "/about.html";
         });
     }
 
     const projects = [
-        { id: "laboratoria-btn", url: "../projects/work/laboratoria.html"},
+        { id: "laboratoria-btn", url: "/projects/work/laboratoria.html"},
         { id: "memory-btn", url: "/projects/work/memory.html"},
         { id: "atip-btn", url: "/projects/work/atip.html"}
     ];
