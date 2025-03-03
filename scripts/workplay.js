1db064ddf0cd79e8a748aabbae3e70a9b5dca82b
ElisaCarrascoLanusse <ecarrascolanusse@gmail.com>
2025-03-02 20:18:58 -0600

    03/02/25 - 8:18PM Added more project pages, set up new JS for the Work and Play pages

---
+++ scripts/workplay.js
@@ -0,0 +1,35 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const workProjects = [
+        { id: "laboratoria-btn", url: "projects/work/laboratoria.html"},
+        { id: "memory-btn", url: "projects/work/memory.html"},
+        { id: "atip-btn", url: "projects/work/atip.html"}
+    ];
+
+    const viewWorkButtons = document.querySelectorAll(".button");
+    viewWorkButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const project = workProjects.find(p => p.id === button.id);
+            if (project) {
+                window.location.href = project.url;
+            }
+        });
+    });    
+
+
+    const playProjects = [
+        { id: "theology-btn", url: "projects/play/theology.html"}
+    ];
+
+    const viewPlayButtons = document.querySelectorAll(".button");
+    viewPlayButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const project = playProjects.find(p => p.id === button.id);
+            if (project) {
+                window.location.href = project.url;
+            }
+        });
+    });    
+});
+
