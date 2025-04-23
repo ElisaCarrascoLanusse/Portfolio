c0a6888ca84c8e542603e8a921a4e8007e1dedd3
“ElisaCarrascoLanusse” <ecarrascolanusse@gmail.com>
2025-04-23 17:30:15 -0500

    Work other projects update

--- scripts/workplay.js
+++ scripts/workplay.js
@@ -1,6 +1,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+    // Work
     const workProjects = [
         { id: "laboratoria-btn", url: "projects/work/laboratoria.html"},
         { id: "memory-btn", url: "projects/work/memory.html"},
@@ -17,7 +18,35 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });    
 
+    // Work Other Projects
+        // Set buttons
+        const presentationsButton = document.querySelector("#presentations-btn"); 
+        const graphicsButton = document.querySelector("#graphics-btn");
 
+        // Set presentations and graphics sections
+        const presentations = document.querySelector("#presentations");
+        const graphics = document.querySelector("#graphics");
+
+        if (presentationsButton && graphicsButton && presentations && graphics) {
+            presentationsButton.addEventListener("click", () => {
+                presentations.classList.remove("hidden");
+                presentations.classList.add("flex");
+
+                graphics.classList.add("hidden");
+                graphics.classList.remove("flex");
+            });
+
+            graphicsButton.addEventListener("click", () => {
+                graphics.classList.remove("hidden");
+                graphics.classList.add("flex");
+
+                presentations.classList.add("hidden");
+                presentations.classList.remove("flex");
+            });
+        }
+
+
+    // Play
     const playProjects = [
         { id: "theology-btn", url: "projects/play/theology.html"}
     ];
