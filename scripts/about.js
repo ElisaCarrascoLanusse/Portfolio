4ff58abaf17bf10d168b53ffaad5fec86716e85c
ElisaCarrascoLanusse <ecarrascolanusse@gmail.com>
2025-12-05 16:38:54 -0500

    Updated cv

--- scripts/about.js
+++ scripts/about.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const cvButton = document.querySelector("#cv-btn");
     if (cvButton) {
         cvButton.addEventListener("click", () => {
-            window.open("https://drive.google.com/file/d/1a0vl8cgyC5yo9xYC5vg5SItyhtk_sNSg/view?usp=sharing", "_blank");
+            window.open("https://drive.google.com/file/d/1xfrwjXx6tT64fMnJw1VoRSJ92P-7Hhs6/view?usp=sharing", "_blank");
         });
     }
 });
